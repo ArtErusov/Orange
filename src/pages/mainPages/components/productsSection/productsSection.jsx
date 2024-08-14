074983c9b17b0812d28512ecc9e81376d2fc591a
Artem Erusov <erusovav@gmail.com>
2024-08-14 17:51:08 +0300

    I made a grid and placed the cards, plus small fixes

--- src/pages/mainPages/components/productsSection/productsSection.jsx
+++ src/pages/mainPages/components/productsSection/productsSection.jsx
@@ -1,6 +1,8 @@
 import styles from './styles.module.scss';
 import SelectСategory from './components/selectCategory/selectСategory';
 import SelectSort from './components/selectSort/selectSort';
+import data from '../../../../data.json';
+import MainCard from '../../../../components/ui/card/mainCard/mainCard';
 
 const ProductsSection = () => {
   return (
@@ -9,6 +11,11 @@ const ProductsSection = () => {
         <div className={styles.flex}>
           <SelectСategory />
           <SelectSort />
+        </div>
+        <div className={styles.gridCont}>
+          {data.map((item) => (
+            <MainCard key={item.id} data={item} />
+          ))}
         </div>
       </div>
     </>
