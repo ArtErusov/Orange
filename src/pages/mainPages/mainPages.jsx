d683a6d20640b52022d7514ec9477a44421dc142
erusovav <erusovav@gmail.com>
2024-08-26 14:32:17 +0000

    “test”

--- src/pages/mainPages/mainPages.jsx
+++ src/pages/mainPages/mainPages.jsx
@@ -1,7 +1,7 @@
 import Banner from './components/bannerSection/banner';
 import ProductsSection from './components/productsSection/productsSection';
 
-feath("https://65523e2c5c69a7790329c0eb.mockapi.io/items");
+// feath("https://65523e2c5c69a7790329c0eb.mockapi.io/items");
 
 const MainPages = () => {
   return (
