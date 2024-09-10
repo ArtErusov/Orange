2327720ae81a11c018c7a209432afe87c99c7fa0
Artem Erusov <erusovav@gmail.com>
2024-09-10 15:15:34 +0300

    Added a clear button and passed the values ​​from the search to the main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import Header from './components/header/header';
 import MainPages from './pages/mainPages/mainPages';
 import NotFound from './pages/notFound/notFound';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [searchVavue, setSearchValue] = useState('');
+  const handleSearchVavue = (item) => setSearchValue(item);
+  console.log(searchVavue + '   fsdf')
   return (
     <>
-      <Header/>
+      <Header searchVavue={searchVavue} handleSearchVavue={handleSearchVavue}/>
       <Routes>
         <Route path="/" element={<MainPages />} />
         <Route path="*" element={<NotFound />} />
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 
 import TopHeader from './components/topHeader/topHeader';
 import Catalog from './components/catalog/catalog';
+import closeIcon from '/img/svg/closeIcons.svg';
 
 import logoHeader from '/img/svg/logoHeader.svg';
 import searchIcon from '/img/svg/searchIcon.svg';
@@ -9,28 +10,33 @@ import favouritesIconGray from '/img/svg/favouritesIconGray.svg';
 import basketIcon from '/img/svg/basketIcon.svg';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ searchVavue, handleSearchVavue }) => {
   return (
     <>
       <TopHeader />
-      <div className={styles.container + " " + styles.header}>
+      <div className={styles.container + ' ' + styles.header}>
         <Link to="/">
-        <img className={styles.header_logo} src={logoHeader} alt="" />
+          <img className={styles.header_logo} src={logoHeader} alt="" />
         </Link>
         <div className={styles.header_catalog}>
           <Catalog />
         </div>
-        <div  className={styles.position}>
-          <input className={styles.header_search} type="text" placeholder='Найти товар' />
-          <div className={styles.header_search_icon}><img src={searchIcon} alt="" /></div>
+
+        <div className={styles.position}>
+          <input value={searchVavue} onChange={(event) => handleSearchVavue(event.target.value)} className={styles.header_search} type="text" placeholder="Найти товар" />
+          {searchVavue && <img onClick={() => handleSearchVavue('')} className={styles.header_search_close} src={closeIcon} alt="" />}
+          <div className={styles.header_search_icon}>
+            <img src={searchIcon} alt="" />
+          </div>
         </div>
+
         <div className={styles.header_choice}>
           <div className={styles.header_choice_favorites}>
             <img src={favouritesIconGray} alt="" />
             <p>избранное</p>
           </div>
           <div className={styles.header_choice_basket}>
-          <img src={basketIcon} alt="" />
+            <img src={basketIcon} alt="" />
             <p>корзина</p>
           </div>
         </div>
