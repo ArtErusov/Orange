9665d0bc29b6b162dad7dedd2915c2df1ed70d5d
Artem Erusov <erusovav@gmail.com>
2024-09-15 20:44:04 +0300

    Started testing Redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Header from './components/header/header';
 import MainPages from './pages/mainPages/mainPages';
 import NotFound from './pages/notFound/notFound';
 import { Routes, Route } from 'react-router-dom';
+import  configureStore  from '../redux/store';
 
 function App() {
   const [searchVavue, setSearchValue] = useState('');
   const handleSearchVavue = (item) => setSearchValue(item);
-  console.log(searchVavue + '   fsdf')
+  console.log("configureStore")
   return (
     <>
       <Header searchVavue={searchVavue} handleSearchVavue={handleSearchVavue}/>
